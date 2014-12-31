9d7d968a64c5e46c85a5f7c305479e120b7d8585
Lowe Schmidt <git@loweschmidt.se>
2014-12-31 21:20:16 +0100

    Multiple ways of installing package. Remove site specific old code

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,43 +1,49 @@
 class gigaspaces::package {
   
-  file { 'oracle_jdk':
-    ensure  => present,
-    path    => '/opt/jdk-7u71-linux-x64.rpm',
-    source  => "puppet:///modules/${module_name}/jdk-7u71-linux-x64.rpm",
+  $unpack_dir    = dirname($::gigaspaces::home_dir)
+  $file_name     = "${::gigaspaces::package_base_name}-${::gigaspaces::package_version}-ga-${::gigaspaces::package_build}.zip"
+  $install_path  = "${unpack_dir}/${::gigaspaces::package_base_name}-${::gigaspaces::package_version}-ga"
+  
+  if $::gigaspaces::manage_java {
+    package { 'java':
+      ensure => installed,
+    }
   }
+  
+  if ( $::gigaspaces::manage_package and $::gigaspaces::package_provider == 'zip') {
+    package { 'unzip':
+      ensure => present,
+    }
 
-  exec { 'install_oracle_jdk':
-    path    => '/bin',
-    cwd     => '/opt',
-    command => 'rpm -iv jdk-7u71-linux-x64.rpm',
-    creates => '/usr/java/',
-    require => File['oracle_jdk'],
-  }
+    file { 'gigaspaces_zip':
+      ensure  => file,
+      path    => "${install_path}-${::gigaspaces::package_build}.zip",
+      source  => "puppet:///modules/${module_name}/${file_name}",
+      notify  => Exec['unzip_gigaspaces'],
+      require => Package['unzip'],
+    }
 
-  package { 'unzip':
-    ensure => present,
-  }
+    exec { 'unzip_gigaspaces':
+      command => "unzip ${unpack_dir}/${file_name}",
+      cwd     => $unpack_dir,
+      path    => '/usr/bin',
+      creates => "${install_path}",
+      require => File['gigaspaces_zip'],
+    }
 
-  $gs_basename = 'gigaspaces-xap-premium'
+  } elsif ($::gigaspaces::manage_package and $::gigaspaces::package_provider == 'os_package') {
 
-  file { 'gigaspaces_zip':
-    ensure  => file,
-    path    => "/opt/${gs_basename}-${package_version}-ga-${package_build}.zip",
-    source  => "puppet:///modules/${module_name}/${gs_basename}-${package_version}-ga-${package_build}.zip",
-    notify  => Exec['unzip_gigaspaces'],
-    require => Package['unzip'],
+    package { 'gigaspaces':
+      ensure => present,
+    }
+  } else {
+    notice ("You have opted out of managing the ${module_name} package from this module.")
   }
-  exec { 'unzip_gigaspaces':
-    command => "unzip /opt/${gs_basename}-${package_version}-ga-${package_build}.zip",
-    cwd     => '/opt/',
-    path    => '/usr/bin',
-    creates => "/opt/${gs_basename}-${package_version}-ga",
-    require => File['gigaspaces_zip'],
-  }
+  
 
   file { 'gigaspaces':
     ensure => link,
-    name   => '/opt/gigaspaces',
-    target => "/opt/${gs_basename}-${package_version}-ga"
+    name   => $::gigaspaces::home_dir,
+    target => $install_path,
   }
 }
