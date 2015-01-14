40bf637f32d6c67d608e500f35a8b318f0e52865
Lowe Schmidt <git@loweschmidt.se>
2015-01-14 14:34:09 +0100

    params class history rewritten to remove license key

---
+++ manifests/params.pp
@@ -0,0 +1,25 @@
+class gigaspaces::params {
+  $environment_file    = "${home_dir}/environment.sh"
+  $group               = 'gigaspaces'
+  $home_dir            = '/opt/gigaspaces'
+  $java_home           = '/usr/java/default'
+  $lib_dir             = "${home_dir}/lib"
+  $license_key         = ''
+  $log_dir             = "${home_dir}/logs"
+  $lookup_groups       = []
+  $lookup_locators     = []
+  $lookup_port         = 4174
+  $manage_java         = true
+  $manage_license      = false
+  $manage_package      = true
+  $manage_service      = true
+  $manage_user         = true
+  $management_machine  = false
+  $package_base_name   = 'gigaspaces-xap-premium'
+  $package_build       = 'b11800'
+  $package_provider    = 'zip'
+  $package_version     = '10.0.1'
+  $password            = 'gigaspaces'
+  $user                = 'gigaspaces'
+  
+}
